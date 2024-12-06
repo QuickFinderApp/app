b119f5cb58834e9b228f2c4722eea7047de2d8ce
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2024-12-06 18:03:03 +0000

    initial

--- backend/src/ipc/apps/apps.ts
+++ backend/src/ipc/apps/apps.ts
@@ -1,11 +1,14 @@
 import { getLinuxApplications } from "./linux";
 import { getMacApplications } from "./macOS";
+import { getWindowsApplications } from "./windows";
 
 export async function getApplications(): Promise<{ name: string; icon: string; path: string }[]> {
   if (process.platform == "darwin") {
     return await getMacApplications();
   } else if (process.platform == "linux") {
     return await getLinuxApplications();
+  } else if (process.platform == "win32") {
+    return await getWindowsApplications();
   }
   return [];
 }
---
+++ backend/src/ipc/apps/windows.ts
@@ -0,0 +1,3 @@
+export async function getWindowsApplications(): Promise<{ name: string; icon: string; path: string }[]> {
+  return [];
+}
